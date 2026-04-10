3655baaf42091566cfc694dbf5e774132a1794c2
rez <burningcode16@gmail.com>
2026-04-10 17:28:43 +0530

    Setup API Helpers for easier referencing

--- pokedex-init/src/util/api.ts
+++ pokedex-init/src/util/api.ts
@@ -1,5 +1,34 @@
-const BASE_URL = "https://pokeapi.co/api/v2/";
+const BASE_URL = "https://pokeapi.co/api/v2";
 
-export async function getPokemonList() {
-    return await fetch(BASE_URL + "pokemon");
+interface Pokemon {
+    name: string;
+    url: string;
+}
+
+interface PokemonResult {
+    count: number;
+    next: string;
+    previous: string;
+    results: Pokemon[]
+}
+
+export async function getPokemonList() : Promise<PokemonResult[]> {
+    const res = await fetch(BASE_URL + "/pokemon");
+    if (!res.ok) return [];
+    const json = await res.json();
+    return json;
+}
+
+export async function getPokemonDetailsByUrl(url: string) {
+    const res = await fetch(url);
+    if (!res.ok) return [];
+    const json = await res.json();
+    return json;
+}
+
+export async function getPokemonDetailsById(id: string) {
+    const res = await fetch(`${BASE_URL}/pokemon/${id}`);
+    if (!res.ok) return [];
+    const json = await res.json();
+    return json;
 }
