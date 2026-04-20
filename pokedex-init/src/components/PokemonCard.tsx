2c80f804f3dde2d12db511427207e64a979e1df5
rez <burningcode16@gmail.com>
2026-04-20 17:03:49 +0530

    Added todos to project

--- pokedex-init/src/components/PokemonCard.tsx
+++ pokedex-init/src/components/PokemonCard.tsx
@@ -1,7 +1,11 @@
+import { ReactElement } from 'react';
+
 export interface PokemonCardProps {
     // TODO: define the required properties with types here
 }
 
-export default function PokemonCard(props: PokemonCardProps) {
-    return <div></div>;
+export default function PokemonCard(props: PokemonCardProps) : ReactElement {
+    return <div>
+        {/*TODO: Pokemon Card Layout with Image and Details */}
+    </div>;
 }
--- pokedex-init/src/components/PokemonList.tsx
+++ pokedex-init/src/components/PokemonList.tsx
@@ -1,7 +1,9 @@
+import { ReactElement } from "react";
+
 export interface PokemonListProps {
-    
+    // TODO: List Component Prop Definitions go here
 }
 
-export default function PokemonList(props: PokemonListProps) {
+export default function PokemonList(props: PokemonListProps) : ReactElement {
     return <div></div>;
 }
